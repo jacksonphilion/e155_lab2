cb91e6449ecbe33c86c8ce038d9d5aab3e6dccad
Jackson <jacksonphilion@gmail.com>
2024-09-12 18:07:02 -0700

    Final draft of code with fixed timer and hardware

--- FPGA/radiant_lab2/source/impl_1/top.sv
+++ FPGA/radiant_lab2/source/impl_1/top.sv
@@ -23,7 +23,7 @@ module top(
 
 endmodule
 
-module displayMultiplexer #(parameter displayFreqHz=1000) (
+module displayMultiplexer #(parameter displayFreqHz=1) (
     input   logic   [7:0]   switch,
     input   logic           reset,
     output  logic   [6:0]   segment,
@@ -40,30 +40,63 @@ module displayMultiplexer #(parameter displayFreqHz=1000) (
     Note that toggle[1] corresponds to Display L, and [0] to R. */
 
     logic           toggleFreq;
-    logic   [6:0]   intSegL;
-    logic   [6:0]   intSegR;
+    logic   [3:0]   intSwitch;
     logic   [1:0]   toggle;
 
-    sevenSegLogic   segRightCall (switch[3:0], intSegR);
-    sevenSegLogic   segLeftCall  (switch[7:4], intSegL);
-
-    frequencyGenerator #(.divisionFactor(48000), .halfFactor(24000), .counterBits(20)) freqGenCall (reset, toggleFreq);
+    frequencyGenerator #(.divisionFactor(48000)) freqGenCall (reset, toggleFreq);
 
     always_ff @(posedge toggleFreq)
-    // If reset is high (active low reset), and we were displaying the R screen with toggle=01
-    // The switch to display things on the left
-    if (reset & (~toggle[1]) & toggle[0]) begin
-        toggle <= 2'b10;
-        segment <= intSegL;
-    end
-    else begin
+    // If reset is low (active low reset), or we were displaying the L screen, switch to R with toggle=01
+    if ((~reset) | (toggle[1]&(~toggle[0]))) begin
         toggle <= 2'b01;
-        segment <= intSegR;
+        intSwitch <= ~(switch[3:0]);
+    end
+	// Otherwise, if not reset and toggle is showing R screen, switch to L toggle=10
+    else begin
+        toggle <= 2'b10;
+        intSwitch <= ~(switch[7:4]);
     end
 
     assign displayL = ~toggle[1];
     assign displayR = ~toggle[0];
+	
+	sevenSegLogic segLogicCall(intSwitch, segment);
 
+endmodule
+
+module frequencyGenerator #(parameter divisionFactor=24000000) (
+    input   logic   reset,
+    output  logic   desiredFreqOut
+    );
+    // This module is coded to output a default freq of 2.4Hz. The factors above
+    // may be changed in the module call to adjust this.
+    
+    // High Frequency 48MHz Oscillator
+    logic int_osc;
+    HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+
+    // Oscillator-based Counter which gets counter on desired frequency (48.0 MHz/divisionFactor)
+    logic [31:0] counter = 0;
+    always_ff @(posedge int_osc)
+        if (~(reset)) counter <= 0;
+        else if (counter < divisionFactor) counter <= counter + 1;
+        else    counter <= 0;
+    
+    // Get desired one bit frequency output from counter
+    always_ff @(posedge int_osc)
+        if (counter > (divisionFactor/2)) desiredFreqOut <= 1;
+        else desiredFreqOut <= 0;
+endmodule
+
+module ledLogic(
+    input   logic   [7:0]   switch,
+    output  logic   [4:0]   led
+    );
+    // This module encodes the 5 onboard LEDs to display the sum of
+    // two hex numbers, encoded as 2 4-bit numbers by 8 total switches.
+	logic [7:0] nSW;
+	assign nSW = ~switch;
+    assign led[4:0] = nSW[7:4] + nSW[3:0];
 endmodule
 
 module sevenSegLogic(
@@ -92,38 +125,4 @@ module sevenSegLogic(
             4'hf: segment <= ~7'b0010111;
             default: segment <= ~7'b0000001;
         endcase
-endmodule
-
-module frequencyGenerator #(parameter divisionFactor=20000000, parameter halfFactor=10000000, parameter counterBits=25) (
-    input   logic   reset,
-    output  logic   desiredFreqOut
-    );
-    // This module is coded to output a default freq of 2.4Hz. The factors above
-    // may be changed in the module call to adjust this.
-    
-    // High Frequency 48MHz Oscillator
-    logic int_osc;
-    HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-
-    // Oscillator-based Counter which gets counter on desired frequency (48.0 MHz/divisionFactor)
-    logic [(counterBits-1):0] counter = 0;
-    always_ff @(posedge int_osc)
-        if (~(reset)) counter <= 0;
-        else if (counter < divisionFactor) counter <= counter + 1;
-        else    counter <= 0;
-    
-    // Get desired one bit frequency output from counter
-    always_comb
-        if (counter > halfFactor) desiredFreqOut = 1;
-        else desiredFreqOut = 0;
-endmodule
-
-module ledLogic(
-    input   logic   [7:0]   switch,
-    output  logic   [4:0]   led
-    );
-    // This module encodes the 5 onboard LEDs to display the sum of
-    // two hex numbers, encoded as 2 4-bit numbers by 8 total switches.
-
-    assign led[4:0] = switch[7:4] + switch[3:0];
 endmodule
